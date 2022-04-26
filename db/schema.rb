623ec10081df17ef9a4410593208c4d7ec73c9af
izumi.nishioka <izumi.n.mail@gmail.com>
2022-04-26 12:30:33 +0000

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_26_102451) do
+ActiveRecord::Schema.define(version: 2022_04_26_121839) do
 
   create_table "tasks", charset: "utf8mb4", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
   end
 
 end
